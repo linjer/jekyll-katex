81509129c4bbd442f2e3c24f3ede18276eb06a25
Jerry Lin <linjer@gmail.com>
2018-09-25 23:36:47 -0700

    Update dependencies, address style

--- lib/jekyll-katex/katex_js.rb
+++ lib/jekyll-katex/katex_js.rb
@@ -5,6 +5,7 @@ require 'jekyll-katex/configuration'
 
 # Holds reference to the katex.min.js lib
 module Jekyll
+  # Katex fast math rendering, see https://github.com/Khan/KaTeX
   module Katex
     KATEX_JS ||= ExecJS.compile(File.open(Jekyll::Katex::Configuration.js_path).read)
   end
